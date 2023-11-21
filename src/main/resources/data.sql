adcb8a3c5941942758994ae16e86253b5e9a492f
Alan Roza <alancruz@outlook.com>
2023-11-20 22:27:00 -0300

    fix: jenkins file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,14 @@
+-- create table users
+CREATE TABLE users (
+    id serial not null,
+    blocking_datetime timestamp,
+    email varchar(255),
+    fail_tries_counter integer,
+    name varchar(255),
+    password varchar(255),
+    primary key (id)
+);
 -- Insert users
-INSERT INTO users (name, email, password) VALUES
-  ('John Doe', 'john@example.com', 'secure456'),
-  ('test user', 'test@example.com', 'password123');
+INSERT INTO users (name, email, password, fail_tries_counter) VALUES
+  ('John Doe', 'john@example.com', 'secure456', 0),
+  ('test user', 'test@example.com', 'password123', 0);
--- src/test/java/com/projects/userAccountApi/LoginControllerIT.java
+++ src/test/java/com/projects/userAccountApi/LoginControllerIT.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LoginControllerIntegrationTest {
+public class LoginControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
