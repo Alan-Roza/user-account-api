5473b102b6dd3a1435f01d5cf54c02ce78b61674
Alan Roza <alancruz@outlook.com>
2023-11-21 01:20:50 -0300

    fix: jenkins file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
--- create table users
-CREATE TABLE users (
-    id serial not null,
-    blocking_datetime timestamp,
-    email varchar(255),
-    fail_tries_counter integer,
-    name varchar(255),
-    password varchar(255),
-    primary key (id)
-);
+---- create table users
+--CREATE TABLE users (
+--    id serial not null,
+--    blocking_datetime timestamp,
+--    email varchar(255),
+--    fail_tries_counter integer,
+--    name varchar(255),
+--    password varchar(255),
+--    primary key (id)
+--);
 -- Insert users
 INSERT INTO users (name, email, password, fail_tries_counter) VALUES
   ('John Doe', 'john@example.com', 'secure456', 0),
