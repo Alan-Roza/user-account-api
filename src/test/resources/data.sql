88ad433afdb7f0b973eef075b83647fd215690fb
Alan Roza <alancruz@outlook.com>
2023-11-23 00:53:11 -0300

    fix: jenkins file

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    fail_tries_counter INTEGER,
+    blocking_datetime TIMESTAMP
+);
+
 INSERT INTO users(name, email, password, fail_tries_counter) VALUES('macarrao', 'macarrao@gmail.com', 'meumacarrao', 0);
