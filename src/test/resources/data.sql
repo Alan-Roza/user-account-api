75b1385ac24ac68befe83deedd568a6f442cf820
Alan Roza <alancruz@outlook.com>
2023-11-23 01:15:03 -0300

    fix: jenkins file

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO users(name, email, password, fail_tries_counter, blocking_datetime) VALUES
-    ('John Doe', 'john@example.com', 'secure456', 0, NULL),
+    ('John Doe', 'john@example.com', 'secure4567', 0, NULL),
     ('test user', 'test@example.com', 'password123', 0, NULL);
